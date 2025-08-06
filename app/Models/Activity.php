b4af6cd6d1dd749955452dbfbe22eb3464279759
Seanneskie <91924632+Seanneskie@users.noreply.github.com>
2025-08-06 18:26:28 +0800

    Add casts for Activity attributes

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -21,6 +21,19 @@ class Activity extends Model
         'photo_path',
     ];
 
+    /**
+     * Cast attributes to common types.
+     */
+    protected function casts(): array
+    {
+        return [
+            'scheduled_at' => 'datetime',
+            'budget' => 'float',
+            'latitude' => 'float',
+            'longitude' => 'float',
+        ];
+    }
+
     public function itinerary()
     {
         return $this->belongsTo(Itinerary::class);
